5a63a007325144344c1d892c7b6fe986c249e537
MarkYu98 <markyu@pku.edu.cn>
2019-06-21 23:20:29 +0800

    ex3

--- kern/e1000.h
+++ kern/e1000.h
@@ -1,3 +1,11 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
+
+#include <kern/pci.h>
+
+#define E1000_VENDOR_ID 0x8086
+#define E1000_DEVICE_ID 0x100E
+
+int e1000_attach(struct pci_func *pcif);
+
 #endif  // SOL >= 6
